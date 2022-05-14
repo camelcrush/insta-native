630b4dd16044e0edb5edadc40867a87a08f4d0ef
camelcrush <dkxm0927@naver.com>
2022-05-14 11:17:47 +0900

    AsyncStorage: save token & preload token

--- App.js
+++ App.js
@@ -7,14 +7,15 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Appearance } from "react-native";
 import LoggedOutNav from "./navigators/LoggedOutNav";
 import { ApolloProvider, useReactiveVar } from "@apollo/client";
-import client, { isLoggedInVar } from "./apollo";
+import client, { isLoggedInVar, tokenVar } from "./apollo";
 import LoggedInNav from "./navigators/LoggedInNav";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function App() {
   const [loading, setLoading] = useState(true);
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const onFinish = () => setLoading(false);
-  const preload = () => {
+  const preloadAssets = () => {
     const fontsToLoad = [Ionicons.font];
     const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
     const imagesToLoad = [
@@ -23,6 +24,14 @@ export default function App() {
     ];
     const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
     return Promise.all([...fontPromises, ...imagePromises]);
+  };
+  const preload = async () => {
+    const token = await AsyncStorage.getItem("token");
+    if (token) {
+      isLoggedInVar(true);
+      tokenVar(token);
+    }
+    return preloadAssets();
   };
   if (loading) {
     return (
--- apollo.js
+++ apollo.js
@@ -1,6 +1,24 @@
 import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const isLoggedInVar = makeVar(false);
+export const tokenVar = makeVar("");
+
+export const logUserIn = async (token) => {
+  await AsyncStorage.multiSet([
+    ["token", token],
+    ["loggedIn", "yes"],
+  ]);
+  isLoggedInVar(true);
+  tokenVar(token);
+};
+
+export const logUserOut = async () => {
+  const keys = ["token", "loggedIn"];
+  await AsyncStorage.multiRemove(keys);
+  isLoggedInVar(false);
+  tokenVar("");
+};
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",

60c7f92523557befef4a8bac086df771b1ddabdd
camelcrush <dkxm0927@naver.com>
2022-05-19 20:55:09 +0900

    Select Photo 3

--- screens/SelectPhoto.js
+++ screens/SelectPhoto.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components/native";
 import * as MediaLibrary from "expo-media-library";
+import { FlatList, Image, useWindowDimensions } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Container = styled.View`
   flex: 1;
@@ -8,19 +10,32 @@ const Container = styled.View`
 `;
 const Top = styled.View`
   flex: 1;
+  background-color: black;
 `;
 
 const Bottom = styled.View`
   flex: 1;
+  background-color: black;
+`;
+
+const ImageContainer = styled.TouchableOpacity``;
+const IconContainer = styled.View`
+  position: absolute;
+  bottom: 5px;
+  right: 0px;
 `;
 
 export default function SelectPhoto({ navigation }) {
+  const numColumns = 4;
+  const { width } = useWindowDimensions();
   const [ok, setOk] = useState(false);
   const [photos, setPhotos] = useState([]);
+  const [chosenPhoto, setChosenPhoto] = useState("");
   const getPhotos = async () => {
     if (ok) {
       const { assets: photos } = await MediaLibrary.getAssetsAsync();
       setPhotos(photos);
+      setChosenPhoto(photos[0]?.uri);
     }
   };
   const getPermissions = async () => {
@@ -40,10 +55,38 @@ export default function SelectPhoto({ navigation }) {
   useEffect(() => {
     getPermissions();
   }, []);
+  const choosePhoto = (uri) => {
+    setChosenPhoto(uri);
+  };
+  const renderItem = ({ item: photo }) => (
+    <ImageContainer onPress={() => choosePhoto(photo.uri)}>
+      <Image
+        source={{ uri: photo.uri }}
+        style={{ width: width / numColumns, height: 100 }}
+      />
+      <IconContainer>
+        <Ionicons name="checkmark-circle" size={18} color="white" />
+      </IconContainer>
+    </ImageContainer>
+  );
   return (
     <Container>
-      <Top />
-      <Bottom />
+      <Top>
+        {chosenPhoto !== "" ? (
+          <Image
+            source={{ uri: chosenPhoto }}
+            style={{ width, height: "100%" }}
+          />
+        ) : null}
+      </Top>
+      <Bottom>
+        <FlatList
+          numColumns={numColumns}
+          data={photos}
+          keyExtractor={(photo) => photo.id}
+          renderItem={renderItem}
+        />
+      </Bottom>
     </Container>
   );
 }
